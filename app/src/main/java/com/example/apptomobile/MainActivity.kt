0d2715ce33b8aa7ac1c1d22c79ddd6efe361cc2f
Michael Farias de Lima (UFC) <michaelfarias@insightlab.ufc.br>
2024-11-05 20:41:37 -0300

    first commit

---
+++ app/src/main/java/com/example/apptomobile/MainActivity.kt
@@ -0,0 +1,91 @@
+package com.example.apptomobile
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.apptomobile.ui.theme.AppToMobileTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Mensagem de log e Toast para onCreate
+        Log.d("LifecycleDemo", "onCreate chamado")
+        Toast.makeText(this, "onCreate chamado", Toast.LENGTH_SHORT).show()
+        setContent {
+            AppToMobileTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // Mensagem de log e Toast para onStart
+        Log.d("LifecycleDemo", "onStart chamado")
+        Toast.makeText(this, "onStart chamado", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // Mensagem de log e Toast para onResume
+        Log.d("LifecycleDemo", "onResume chamado")
+        Toast.makeText(this, "onResume chamado", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        // Mensagem de log e Toast para onPause
+        Log.d("LifecycleDemo", "onPause chamado")
+        Toast.makeText(this, "onPause chamado", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        // Mensagem de log e Toast para onStop
+        Log.d("LifecycleDemo", "onStop chamado")
+        Toast.makeText(this, "onStop chamado", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // Mensagem de log e Toast para onDestroy
+        Log.d("LifecycleDemo", "onDestroy chamado")
+        Toast.makeText(this, "onDestroy chamado", Toast.LENGTH_SHORT).show()
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AppToMobileTheme {
+        Greeting("Android")
+    }
+}
+
+
